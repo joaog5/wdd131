7026c29a22d9775a02a9208389eaf521ead5ca15
João <168314703+joaog5@users.noreply.github.com>
2025-11-12 20:13:37 -0300

    Add files via upload

---
+++ week02/script/temples.js
@@ -0,0 +1,9 @@
+
+const yearSpan = document.getElementById("year");
+const lastModifiedSpan = document.getElementById("lastModified");
+
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastModified = document.lastModified;
+lastModifiedSpan.textContent = lastModified;
