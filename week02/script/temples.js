8a784f51e69ba8a70692729ecdc250c4142f9130
João <168314703+joaog5@users.noreply.github.com>
2025-11-13 20:18:58 -0300

    Delete week02 directory

--- week02/script/temples.js
+++
@@ -1,9 +0,0 @@
-
-const yearSpan = document.getElementById("year");
-const lastModifiedSpan = document.getElementById("lastModified");
-
-const currentYear = new Date().getFullYear();
-yearSpan.textContent = currentYear;
-
-const lastModified = document.lastModified;
-lastModifiedSpan.textContent = lastModified;
